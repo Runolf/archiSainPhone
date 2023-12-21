7c8d1ebdde45a2782483c907bafd83e10fa01523
Runolf <alexandreliskiewicz@hotmail.com>
2023-12-21 12:01:56 +0100

    Create actionTypes.js

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,19 @@
+//               GOALS
+
+export const GOAL_LIST      = "GOAL_LIST";
+export const GOAL_CREATE    = "GOAL_CREATE";
+export const GOAL_UPDATE    = "GOAL_UPDATE";
+
+//               CHALLENGE
+
+export const CHALLENGE_LIST     = "CHALLENGE_LIST";
+export const CHALLENGE_CREATE   = "CHALLENGE_CREATE";
+export const CHALLENGE_UPDATE   = "CHALLENGE_UPDATE";
+
+//               USERS
+
+export const ROLE_LIST     = "ROLE_LIST";
+export const LIST_USER     = "LIST_USER";
+export const FIND_USER     = "FIND_USER";
+export const USER_CREATE   = "USER_CREATE";
+export const USER_UPDATE   = "USER_UPDATE";
