0416c109b32ed4f99becc8c5ac38195780df89ee
Runolf <alexandreliskiewicz@hotmail.com>
2024-01-03 11:41:34 +0100

    remove ReduxTest from LandingPage

--- src/Screens/Landing/index.js
+++ src/Screens/Landing/index.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, Pressable } from 'react-native';
 import CreateAccount from './createAccount';
 import ConnectAccount from './connectAccount';
-import ReduxTest from '../TryComponent/reduxTest';
-
 const LandingPage = ({navigation}) => {
 
     const [hasAccount, setHasAccount] = useState(false);
@@ -14,8 +12,6 @@ const LandingPage = ({navigation}) => {
 
     return (
         <View style={styles.container}>
-
-            <ReduxTest />
 
             {hasAccount ? 
             <>
