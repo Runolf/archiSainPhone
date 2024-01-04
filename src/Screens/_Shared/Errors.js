0461b8eb5032a3ab3b35660fd2b08b1469b4fa6d
Runolf <alexandreliskiewicz@hotmail.com>
2024-01-04 15:23:12 +0100

    create Errors component

---
+++ src/Screens/_Shared/Errors.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+
+const Errors = (errorList) => {
+    
+    const renderitem = ({error}) => {
+        return <Text style={styles.error}>{error}</Text>;
+    }
+    return ( <View style={styles.listError}>
+                <FlatList 
+                    data={errorList}
+                    renderItem={renderitem}
+                />
+            </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    listError: {
+        width: "50%",
+        height: "auto",
+        backgroundColor: "red",
+    },
+    error: {
+        color: "white"
+    }
+});
+ 
+export default Errors;
